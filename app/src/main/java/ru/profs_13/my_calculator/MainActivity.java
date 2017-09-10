f46c6498c963075b7673fc9fbe3c385bb1e87015
George <ya.profs@yandex.ru>
2017-09-11 01:08:46 +0300

    Importing essential settings.
    Basic elements.

---
+++ app/src/main/java/ru/profs_13/my_calculator/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.profs_13.my_calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
